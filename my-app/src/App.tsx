2482ed71c8895d371b097a3e6036dfd37480f42b
FunniNam3 <nguyen.ev03@gmail.com>
2025-01-09 15:43:00 -0600

    Experience added

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -25,10 +25,10 @@ function App() {
         </div>
       </header>
 
-      <div className="AboutMe">
+      <section className="AboutMe">
         <h1 className="AboutMe">About Me</h1>
         <h2 className="AboutMe">Professional</h2>
-        <p className="AboutMe">
+        <p className="ProfAbt">
           Computer Engineering undergraduate at Texas A&M with a strong
           foundation in software development, and problem-solving. Passionate
           about building scalable applications and improving software
@@ -38,7 +38,40 @@ function App() {
         <p className="PersonalAbt">
           Viet College Student that loves to rock climb and solve problems
         </p>
-      </div>
+      </section>
+
+      <section className="Experience-Container">
+        <h1 className="Experience-Container">Experience</h1>
+        <ul className="Experience-Container">
+          <li className="Experience">
+            <div className="Experience">
+              <h2 className="Experience">Yokogawa’s US Technology Center</h2>
+              <h2 className="ExLocat">Spring, TX</h2>
+            </div>
+            <div className="Experience">
+              <h3 className="Experience">
+                Information Technology (IT) Specialist Intern
+              </h3>
+              <h3 className="ExDate">May 2024 – August 2024</h3>
+            </div>
+            <ul>
+              <li>
+                Designed and implemented an inventory management app using Power
+                Apps, Power BI, Power Automate, and Microsoft SharePoint,
+                improving asset tracking and management for 2100+ devices, and
+                developed a Workshop Management App to streamline space
+                allocation and track workers' hours, increasing project
+                efficiency
+              </li>
+              <li>
+                Developed automated scripts using Microsoft Automate to
+                streamline device provisioning, reducing setup time by 60% and
+                improving IT operational efficiency
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
