72516fd6a04eae0d8deac8f5ba0e70c7845e6fe0
FunniNam3 <nguyen.ev03@gmail.com>
2025-04-25 06:20:53 -0500

    small additions

--- my-app/src/Projects.tsx
+++ my-app/src/Projects.tsx
@@ -3,9 +3,21 @@ import "./Projects.css";
 
 function Projects() {
   return (
-    <div>
-      <h1>AHHHHHHHHHHHHHHH</h1>
-    </div>
+    <section className="Projects">
+      <h1 className="Projects">Projects</h1>
+      <div className="Container">
+        <h2 className="ProjectName">Engine 12</h2>
+      </div>
+      <div className="Container">
+        <h2 className="ProjectName">Asset Management System</h2>
+      </div>
+      <div className="Container">
+        <h2 className="ProjectName">Raytracing Engine</h2>
+      </div>
+      <div className="Container">
+        <h2 className="ProjectName">Procedural Planet Builder</h2>
+      </div>
+    </section>
   );
 }
 
